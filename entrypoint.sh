49da5886547e752d846c31589f0942d3c1093ba7
Majoneza <matejbozic1@hotmail.com>
2021-09-27 23:37:26 +0200

    updated11

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,14 @@ USER_EMAIL=$5
 USER_NAME=$6
 COMMIT_MESSAGE=$7
 
-if [[ -z $USER_NAME ]]; then
+if [ -z $USER_NAME ]; then
     USER_NAME=$DESTINATION_GITHUB_USERNAME
 fi
 
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
 
-if [[ ! -d ~/.ssh ]]; then
+if [ ! -d ~/.ssh ]; then
     mkdir ~/.ssh
 fi
 
@@ -34,6 +34,8 @@ git status
 
 git diff-index --quiet HEAD || git commit -a --allow-empty-message --message "$COMMIT_MESSAGE"
 
-git push "git@github.com:$DESTINATION_GITHUB_USERNAME/$DESTINATION_REPOSITORY_NAME.git" HEAD:"$TARGET_BRANCH"
+echo "$GITHUB_WORKSPACE"
+
+#git push "git@github.com:$DESTINATION_GITHUB_USERNAME/$DESTINATION_REPOSITORY_NAME.git" HEAD:"$DESTINATION_TARGET_BRANCH"
 
 rm ~/.ssh/id_rsa
